3567b1d37ede0a0c79349b15f485b5969708b034
Wojt3k94 <wojtekbrozynski@gmail.com>
2024-10-07 12:34:25 +0200

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-document.getElementById('hideButton').addEventListener('click', function() {
-    const portfolioText = document.getElementById('portfolioText');
-    if (portfolioText.style.display === 'none') {
-        portfolioText.style.display = 'block';
-    } else {
-        portfolioText.style.display = 'none';
-    }
-});
+// document.getElementById('hideButton').addEventListener('click', function() {
+//     const portfolioText = document.getElementById('portfolioText');
+//     if (portfolioText.style.display === 'none') {
+//         portfolioText.style.display = 'block';
+//     } else {
+//         portfolioText.style.display = 'none';
+//     }
+// });
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
